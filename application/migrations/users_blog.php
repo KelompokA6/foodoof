aa4e8f11ae8efa6270e5935a97305b02675c023c
abidnurulhakim <abidnurulhakim@gmail.com>
2015-02-21 20:07:08 +0700

    migration users

---
+++ application/migrations/users_blog.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_blog extends CI_Migration {
+
+	public function up()
+	{
+		$this->dbforge->add_field(array(
+			'user_id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'user_email' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '255',
+				'null' => FALSE
+			),
+			'user_name' => array(
+				'type' => 'TEXT',
+				'null' => TRUE,
+			),
+			'user_gender' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '1',
+				'null' => TRUE,
+			),
+			'user_bdate' => array(
+				'type' => 'DATE',
+				'null' => TRUE
+			),
+			'user_phone' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '20',
+				'null' => TRUE,
+			),
+			'user_status' => array(
+				'type' => 'TEXT',
+				'null' => TRUE,
+			),
+			'user_photo' => array(
+				'type' => 'MEDIUMTEXT',
+				'null' => TRUE,
+			),
+			'user_banned' => array(
+				'type' => 'BOOLEAN',
+				'null' => TRUE,
+			),
+			'user_last_activity' => array(
+				'type' => 'TIMESTAMP',
+				'null' => TRUE,
+			),
+		));
+		$this->dbforge->add_key('user_email', TRUE);
+		$this->dbforge->create_table('users');
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('users');
+	}
+}
+?>
