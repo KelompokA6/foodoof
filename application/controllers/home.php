2e27fd3fd1bacf0ccdb61e60f78be23612d456fa
Alfan Nur Fauzan <alpancs@gmail.com>
2015-03-19 13:09:56 +0700

    bikin template view

---
+++ application/controllers/home.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	public function index()
+	{	
+		$this->load->view('template_view');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/template_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Template View for Foodoof</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap-theme.min.css">
+</head>
+<body>
+<div class="jumbotron">Sholat dulu ya</div>
+</body>
+</html>
