b37d245db4f913bfa752b30ba3f10197b41cd68c
Alfan Nur Fauzan <alpancs@gmail.com>
2015-03-20 16:31:43 +0700

    update header lagi

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -4,6 +4,11 @@
   <title>Template View for Foodoof</title>
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap-theme.min.css">
+  <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.1.3.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+    $('.dropdown-toggle').dropdown();
+  </script>
 </head>
 <body>
   <header class="navbar navbar-default navbar-fixed-top">
@@ -15,28 +20,32 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="<?php echo base_url();?>" class="navbar-brand">
-            <img src="<?php echo base_url();?>assets/img/foodoof-vs.png">
-          </a>
-        <!-- <a href="<?php echo base_url();?>" class="nav navbar-nav">
-        <img src="<?php echo base_url();?>assets/img/foodoof-vs.png" alt="Home">
-        </a> -->
+          <a href=""><img class="img-responsive" src="<?php echo base_url();?>assets/img/foodoof-vs.png"/></a>
         </div>
 
         <nav class="collapse navbar-collapse bs-navbar-collapse">
           <ul class="nav navbar-nav">
-            <li>
-              <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
+            <li class="col-md-12 col-md-offset-4">
+              <form class="navbar-form navbar-left col-md-12" role="search">
+                <div class="form-group col-md-7">
                   <input type="text" class="form-control" placeholder="Search">
                 </div>
-                <button type="submit" class="btn btn-default">Submit</button>
+                
+                <!--Default buttons with dropdown menu-->
+                <div class="btn-group col-md-3">
+                    <button type="button" data-toggle="dropdown" class="btn btn-default dropdown-toggle">Resep <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Akun</a></li>
+                        <li><a href="#">Resep</a></li>
+                    </ul>
+                </div>
+                <button type="submit" class="btn btn-success col-md-2">Search</button>
               </form>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li>
-              <button type="button" class="btn btn-default navbar-btn">Login</button>
+              <button type="button" class="btn btn-primary navbar-btn">Login</button>
             </li>
             <li>
               <button type="button" class="btn btn-default navbar-btn">Join</button>
