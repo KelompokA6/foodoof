522451c2ca83294d26d820f3dfac133ed7dca175
ffahmii <ffahmii@gmail.com>
2015-03-20 14:58:17 +0700

    hah
    haha

---
+++ application/views/top.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Top Menu</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap-theme.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/fa/css/font-awesome.min.css">
+ 	</head>
+
+  <body>
+  	<div class="col-md-3 col-md-offset-3">
+  		<ul class="list-group">
+		  <li class="list-group-item">
+		  	Resep 1
+		  	<a href="#" class="thumbnail">
+		      <img src="<?php echo base_url();?>assets/img/resep1.jpg" alt="Nasi Goreng">
+		    </a>
+		    <i class="fa fa-eye"></i>
+		  </li>
+		  <li class="list-group-item">
+		  	Resep 2
+		  </li>
+		  <li class="list-group-item">
+		  	Resep 3
+		  </li>
+		  <li class="list-group-item">
+		  	Resep 4
+		  </li>
+		  <li class="list-group-item">
+		  	Resep 5
+		  </li>
+		</ul>
+  	</div>
+  </body>
+</html>
