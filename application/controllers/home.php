ead0716f68ce392e82ef37589aaabfd30e5eedfc
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-22 12:06:55 +0700

    update

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,7 +4,7 @@ class Home extends CI_Controller {
 	public function index()
 	{	
 		$this->load->view('template_view');
-		$this->load->view('top');
+		//$this->load->view('top');
 	}
 	public function home1(){
 		$this->load->view('home');
--- application/views/template_view.php
+++ application/views/template_view.php
@@ -4,6 +4,7 @@
   <title>Template View for Foodoof</title>
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/t1/bootstrap-theme.css">
+  <link href="/foodoof/assets/plugin/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.1.3.min.js"></script>
   <script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
   <script type="text/javascript">
@@ -54,7 +55,7 @@
         </nav>-->
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
       <div class="navbar-header col-md-2">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".bs-navbar-collapse">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
@@ -65,44 +66,132 @@
         <!-- /.navbar-header -->
 
       <ul class="nav navbar-top-links nav navbar-nav navbar-left col-md-10">
-          <div class="col-md-12 form-inline inline-search">
-              <form class="navbar-form collapse-navbar-search">
-                <div class="col-md-2 form-group col-xs-4 text-right">
-                  <select class="form-control form-group btn-primary selector-search" >
-                    <option> Recipe </option>
-                    <option> Account </option>
-                  </select>
-                </div>
-                <div class="input-group form-group col-md-7 col-xs-8">
-                    <input type="text" class="form-control" placeholder="Search Recipe. Use Comma (,) As Separator For Search By Ingredient"/>
-                    <span class="input-group-btn">
-                        <button class="btn btn-primary" type="submit">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </span>
-                </div>
-                <div class="form-group navbar-collapse text-center btn-log-nav">
-                    <div class="btn-group" role="group" aria-label="">
-                        <button type="button" class="btn btn-success btn-cus">    Login    </button>
-                        <button type="button" class="btn btn-primary btn-cus">    Join    </button>
-                    </div>
-                </div>
-              </form>
-          </div>
-          <!-- /.dropdown -->
+        <div class="col-md-12 form-inline inline-search">
+            <form class="navbar-form collapse-navbar-search">
+              <div class="col-md-2 form-group col-xs-4 text-right">
+                <select class="form-control form-group btn-primary">
+                  <option> Recipe </option>
+                  <option> Account </option>
+                </select>
+              </div>
+              <div class="input-group form-group col-md-7 col-xs-8">
+                  <input type="text" class="form-control" placeholder="Search Recipe. Use Comma (,) As Separator For Search By Ingredient"/>
+                  <span class="input-group-btn">
+                      <button class="btn btn-primary" type="submit">
+                          <i class="fa fa-search"></i>
+                      </button>
+                  </span>
+              </div>
+              <div class="form-group bs-navbar-collapse text-center btn-log-nav">
+                  <div class="btn-group" role="group" aria-label="">
+                      <button type="button" class="btn btn-success btn-cus">    Login    </button>
+                      <button type="button" class="btn btn-primary btn-cus">    Join    </button>
+                  </div>
+              </div>
+            </form>
+        </div>
       </ul>
     </nav>
     <!--</div>
   </header>-->
-    <div class="container" style="padding:120px 65px 0 65px">
+    <div class="container container-mobile">
       <div class="row">
-        <div class="col-md-3 navbar-collapse">
-          sasdjasdjaksjdkl
+        <div class="col-md-3 navbar-default bs-navbar-collapse category-menu">
+          <div class="panel-body" for="category">
+            <div class="col-md-12 col-xs-12 page-header category-list">
+              <a href="#">
+                <div class="col-md-3 col-xs-5">
+                  <img class="img-responsive img-circle" src="<?php echo base_url();?>assets/img/foodoof-vs.png"/>
+                </div>
+                <div class="col-md-8 col-xs-6 category-menu-name">
+                  Masakan Pedas
+                </div>
+                <i class="fa fa-chevron-right pull-right chevron-menu"></i>
+              </a>
+            </div>
+            <div class="col-md-12 col-xs-12 page-header category-list">
+              <a href="#">
+                <div class="col-md-3 col-xs-5">
+                  <img class="img-responsive img-circle" src="<?php echo base_url();?>assets/img/foodoof-vs.png"/>
+                </div>
+                <div class="col-md-8 col-xs-6 category-menu-name">
+                  Sayuran
+                </div>
+                <i class="fa fa-chevron-right pull-right chevron-menu"></i>
+              </a>
+            </div>
+            <div class="col-md-12 col-xs-12 page-header category-list">
+              <a href="#">
+                <div class="col-md-3 col-xs-5">
+                  <img class="img-responsive img-circle" src="<?php echo base_url();?>assets/img/foodoof-vs.png"/>
+                </div>
+                <div class="col-md-8 col-xs-6 category-menu-name">
+                  Chinese Food
+                </div>
+                <i class="fa fa-chevron-right pull-right chevron-menu"></i>
+              </a>
+            </div>
+            <div class="col-md-12 col-xs-12 page-header category-list">
+              <a href="#">
+                <div class="col-md-3 col-xs-5">
+                  <img class="img-responsive img-circle" src="<?php echo base_url();?>assets/img/foodoof-vs.png"/>
+                </div>
+                <div class="col-md-8 col-xs-6 category-menu-name">
+                  French Food
+                </div>
+                <i class="fa fa-chevron-right pull-right chevron-menu"></i>
+              </a>
+            </div>
+          </div>
         </div>
         <div class="col-md-9">
-          salkdsalklas;k;
+          <div class="panel-body col-md-12">
+            <div id="carousel-example-generic col-md-12" class="carousel slide" data-ride="carousel" >
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              </ol>
+
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img src="<?php echo base_url();?>assets/img/01.jpg" class="img-responsive" alt="Coba1">
+                  <div class="carousel-caption">
+                    Perccobaan
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url();?>assets/img/02.jpg" class="img-responsive" alt="...">
+                  <div class="carousel-caption">
+                    Percobaan 2
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="<?php echo base_url();?>assets/img/03.jpg" class="img-responsive" alt="...">
+                  <div class="carousel-caption">
+                    Percobaan 3
+                  </div>
+                </div>
+              </div>
+
+              <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </body>
+  <script type="text/javascript">
+    $('.carousel').carousel();
+  </script>
 </html>
