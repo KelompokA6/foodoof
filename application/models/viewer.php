64735e5d1176ec1e96ba4b72f7111c06b0096794
Alfan Nur Fauzan <alpancs@gmail.com>
2015-03-25 20:39:07 +0700

    nambahi viewer.php sama final_view.php dkk --> I mean css.php and also js.php

--- application/models/viewer.php
+++ application/models/viewer.php
@@ -6,13 +6,25 @@ class Viewer extends CI_Model
   {
     $this->load->library('parser');
     $this->load->library('session');
-    $hasLogin = $this->session->userdata('login_status');
 
-    $data = array();
-    $menubar = $this->parser->parse($hasLogin ? 'menubar_login' : 'menubar', $data, TRUE);  
-    $datacomplete = array(
-            "menubar"=> $menubar,
-            );
-    $this->parser->parse($view, $datacomplete);
+    // buat judul. kalau gak dikasih tau, tulis aja judulnya Foodoof
+    $datafinal['title'] = @$data['title'] ? $data['title'] : 'FoodooF';
+
+    // load css tema 1 (t1) apa tema 2 (t2)?
+    $data['theme'] = $this->session->userdata('theme');
+    $datafinal['css'] = $this->parser->parse('css', $data, TRUE);
+
+    // sudah login? load menubar apa menubar_login?
+    $hasLogin = $this->session->userdata('login_status');
+    $datafinal['menubar'] = $this->parser->parse($hasLogin ? 'menubar_login' : 'menubar', $data, TRUE);
+
+    // konten utama. misal home_view, user_timeline_view, dll. tolong ini dibuat 1 file php aja.
+    $datafinal['content'] = $this->parser->parse($view, $data, TRUE);
+
+    // js di-load di akhir aja gpp. (katanya "best practice" di bootstrap)
+    $datafinal['js'] = $this->parser->parse('js', $data, TRUE);
+
+    // oke, sudah semua? LOAD!
+    $this->parser->parse('final_view', $datafinal);
   }
 }
---
+++ application/views/css.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="/foodoof/assets/css/{theme}/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/foodoof/assets/css/{theme}/bootstrap-theme.css">
+<link href="/foodoof/assets/plugin/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
