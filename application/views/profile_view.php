6772680127857b885c17ad356851f1f89280f719
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-27 22:55:34 +0700

    update menubar login

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -14,16 +14,16 @@
 	          <span class="icon-bar"></span>
 	          <span class="icon-bar"></span>
 	      </button>
-	      <a href="" style="padding-left: 60px;">
+	      <a href="" class="brand-menubar">
 	      	<img class="img-circle" width="75px" src="<?php echo base_url();?>assets/img/foodoof.png" />
 	      </a>
 	      <a href="" class="pull-right btn-navbar-mobile text-center"><i class="fa fa-pencil-square-o fa-2x"></i><br>Write <br>A Recipe</a>
 	  </div>
 	  <ul class="nav navbar-top-links nav navbar-nav navbar-left col-md-10">
-	    <div class="col-md-12 form-inline inline-search" style="padding-left: 0; margin-left: -40px;">
+	    <div class="col-md-12 form-inline inline-search col-menubar">
 	        <form class="navbar-form collapse-navbar-search">
 	          <div class="input-group form-group col-md-8 pull-left" style="padding-left:20px;">
-	              <input type="text" class="form-control" placeholder="Search Recipe By Title" style="width:533px"/>
+	              <input type="text" class="form-control input-search" placeholder="Search Recipe By Title" style="width:533px"/>
 	              <div class="input-group-btn">
 	                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Search By<span class="caret"></span></button>
 	                <ul class="dropdown-menu dropdown-search" role="menu" aria-labelledby="dropdownMenu1">
@@ -32,29 +32,32 @@
 	                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Account</a></li>
 	                </ul>
 	                <span class="btn-group">
-	                  <button class="btn btn-primary" type="submit" style="width:60px; height:33.9915px">
+	                  <button class="btn btn-primary button-group-normal" type="submit" >
 	                     <i class="fa fa-search"></i>
 	                  </button>
 	                </span>
 	              </div>
 	          </div>
-	          <div class="form-group col-md-4" style="padding-left:70px">
-	          	<div class="col-md-2" style="padding:4px;">
+	          <div class="form-group col-md-4 col-menu-user">
+	          	<div class="col-md-2 link-by-icon" >
 	          		<a href="" class="text-center" title="New Recipe">
 		            	<i class="fa fa-pencil-square-o fa-2x"></i>
 		          	</a>
 	          	</div>
-	            <div class="col-md-10 hover-menubar">
-	            	<div class="col-md-3">
-	            		<img class="img-circle img-profile-menubar" src="<?php echo base_url();?>assets/img/01.jpg"/>	
-	            	</div>
-	              
-	              <p class="col-md-4">
-	              	Abid
-	              </p>
-	            	 <button type="button" class="btn btn-danger">Logout</button>
+	            <a href="user/profile">
+	            	<div class="col-md-4 hover-menubar text-left" title="Profile">
+		            	<div class="col-md-6 col-no-padding-left col-no-padding-right div-img-profile-menubar">
+		            		<img class="img-circle img-profile-menubar" src="<?php echo base_url();?>assets/img/01.jpg"/>
+		            	</div>
+			            <div class="col-md-6 col-no-padding" style="line-height:34px">
+			            	Abid
+			            </div>
+		            </div>
+	            </a>
+	            <div class="col-md-2 col-offset-md-2">
+	            	<button type="button" class="btn btn-danger btn-sm">Logout</button>
 	            </div>
-	           
+	           	
 	          </div>
 	        </form>
 	    </div>
@@ -62,9 +65,9 @@
 	</nav>
 	<div class="container container-mobile">
       <div class="row">
-      	<div class="col-md-12" style="padding:20px 0;">
-      		<div class="col-md-4" style="padding-left:0;">
-      			<div class="col-md-12" style="padding-left:0;">
+      	<div class="col-md-12 outer-contain">
+      		<div class="col-md-4 col-no-padding-left">
+      			<div class="col-md-12 col-no-padding-left">
       				<img src="<?php echo base_url();?>assets/img/01.jpg" class="img-responsive img-rounded"/>
       			</div>
       			<div class="col-md-12">
