f07b9bf65b27bcb354b87b06468749a3de2fd146
ffahmii <ffahmii@gmail.com>
2015-03-28 16:46:40 +0700

    update recipe  controller

--- application/controllers/recipecontroller.php
+++ application/controllers/recipecontroller.php
@@ -16,17 +16,31 @@ class RecipeController extends CI_Controller {
 	// ini pake post, lihat registration
 	public function save(){
 		$recipe = new Recipe();
-		$recipe->name = $this->input->post("title");
-		$recipe->description = $this->input->post("description");
-		$recipe->portion = $this->input->post("portion");
-		$recipe->duration = $this->input->post("duration");
-		$recipe->author = $this->input->post("author");
-		if($recipe->save()){
-			echo "Success";
+		$name = $this->input->post("title");
+		$description = $this->input->post("description");
+		$portion = $this->input->post("portion");
+		$duration = $this->input->post("duration");
+		$author = $this->input->post("author");
+		$n = (int) $this->input->post("n");
+		$ingredients = array();
+		for ($i=0; $i < $n; $i++) { 
+			$temp['bahan'] = $this->input->post("bahan".$i."");
+			$temp['takaran'] = $this->input->post("takaran".$i."");
+			$temp['unit'] = $this->input->post("unit".$i."");
+			array_push($ingredients, $temp);
 		}
-		else{
-			echo "Failed";
+		$m = (int) $this->input->post("m");
+		$steps = array();
+		for ($i=0; $i < $m; $i++) { 
+			$temp['no_step'] = $i+1;
+			$temp['description'] = $this->input->post("description".$i."");
+			$temp['photo'] = $this->input->post("photo".$i."");
+			array_push($steps, $temp);
 		}
+		$recipe->saveRecipe($id, $name, $portion, $duration, $description, $last_update, $ingredients, $steps)
+		
+		$this->load->model('viewer');
+		$this->viewer->show('coba_top_recipe', ($SUCCESS=1));
 	}
 
 	// 
@@ -45,15 +59,20 @@ class RecipeController extends CI_Controller {
 		$recipe = new Recipe();
 		$this->load->library(session);
 		$userid = $this->session->userdata("user_id");
-		$data = array();
 		if (empty($userid)){
-			$recipe->getRecipeProfile($id);
-			array_push($data, $recipe);
+			$data['profile']= $recipe->getRecipeProfile($id);
+			$data['ingredients'] = $recipe->getIngredients($id);
+			$data['steps'] = $recipe->getSteps($id);
+			$data['categories']= $recipe->getCategories($id);
 		} else {
-			$recipe->getRecipeProfile($id, $userid);
-			array_push($data, $recipe);
+			$data['profile']= $recipe->getRecipeProfile($id, $userid);
+			$data['ingredients'] = $recipe->getIngredients($id, $userid);
+			$data['steps'] = $recipe->getSteps($id, $userid);
+			$data['categories']= $recipe->getCategories($id, $userid);
 		}
-		print_r($data);
+
+		$this->load->model('viewer');
+		$this->viewer->show('coba_top_recipe', $data);
 	}
 
 	// top resep untuk halaman top Page.
@@ -61,85 +80,59 @@ class RecipeController extends CI_Controller {
 	// kalo buat ditampilin di page sendiri, diambil 10 resep.
 	public function top($isOnHome = true){
 		$recipe = new Recipe();
-		if ($isOnHome === true){
-			$recipe->getTopRecipe(5);
+		if ($isOnHome === 'true'){
+			$data=$recipe->getTopRecipe(5);
 		} else {
-			$recipe->getTopRecipe(10);
+			$data=$recipe->getTopRecipe(10);
 		}
-		$this->load->library('parser');
 		$arr = array();
-		foreach ($recipe as $obj)
-		{
-			$data = array(
-				'judul' => $obj->name,
-				'author' => $obj->author,
-				'date' => $obj->last_update,
-				'rating' => $obj->rating,
-				'views' => $obj->views,
-				'photo' => $obj->photo
-				);
-			array_push($arr, $data);
+		foreach ($data as $obj) {
+			array_push($arr, $obj);
 		}
 		$data1=array(
 			'data' => $arr
 			);
-
+		$this->load->library('parser');
 		$this->parser->parse("coba_top_recipe", $data1);
 	}
 
 	// ini buat ambil highlight resep
 	public function highlight($isOnHome = true){
 		$recipe = new Recipe();
-		$recipe->getHightlight(10);
-		$this->load->library('parser');
+		if ($isOnHome === 'true'){
+			$data=$recipe->getHightlight(5);
+		} else {
+			$data=$recipe->getHightlight(10);
+		}
 		$arr = array();
-		foreach ($recipe as $obj)
-		{
-			$data = array(
-				'judul' => $obj->name,
-				'author' => $obj->author,
-				'date' => $obj->last_update,
-				'rating' => $obj->rating,
-				'views' => $obj->views,
-				'photo' => $obj->photo
-				);
-			array_push($arr, $data);
+		foreach ($data as $obj) {
+			array_push($arr, $obj);
 		}
 		$data1=array(
 			'data' => $arr
 			);
-
+		$this->load->library('parser');
 		$this->parser->parse("coba_top_recipe", $data1);
 	}
 
 	// ini buat ambil recent resep
 	// kalo buat ditampilin di home, cuma ambil 5 resep aja. 
 	// kalo buat ditampilin di page sendiri, diambil 10 resep.
-	public function recent($isOnHome = true){
+	public function recent($isOnHome = 'true'){
 		$recipe = new Recipe();
-		if ($isOnHome === true){
-			$recipe->getRecently(5);
+		if ($isOnHome === 'true'){
+			$data=$recipe->getRecently(5);
 		} else {
-			$recipe->getRecently(10);
+			$data=$recipe->getRecently(10);
 		}
-		$this->load->library('parser');
 		$arr = array();
-		foreach ($recipe as $obj)
-		{
-			$data = array(
-				'judul' => $obj->name,
-				'author' => $obj->author,
-				'date' => $obj->last_update,
-				'rating' => $obj->rating,
-				'views' => $obj->views,
-				'photo' => $obj->photo
-				);
-			array_push($arr, $data);
+		foreach ($data as $obj) {
+			array_push($arr, $obj);
 		}
 		$data1=array(
 			'data' => $arr
 			);
-
+		$this->load->library('parser');
 		$this->parser->parse("coba_top_recipe", $data1);
 	}
 }
