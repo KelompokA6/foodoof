ea8122cb921d27d9e1e0ef76045f81be6054bc8d
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-28 16:13:06 +0700

    update

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -84,22 +84,14 @@
 	                            <input id="login-username" type="text" class="form-control" name="email_user" value="" placeholder="email">                                        
 	                        </div>        
 	                        <div style="margin-bottom: 25px" class="input-group">
-	                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-	                                    <input id="login-password" type="password" class="form-control" name="password_user" placeholder="password">
-	                                </div>
-	                        <div class="input-group">
-	                            <div class="checkbox">
-	                                <label>
-	                                  <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
-	                                </label>
-	                              </div>
-	                            </div>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input id="login-password" type="password" class="form-control" name="password_user" placeholder="password">
+                            </div>	
 	                        <div style="margin-top:10px" class="form-group">
 	                            <!-- Button -->
 
 	                            <div class="col-sm-12 controls">
 	                              <button id="btn-signin" type="submit" class="btn btn-success">Login  </button>
-	                              <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
 	                            </div>
 	                        </div>
 	                        <div class="form-group">
