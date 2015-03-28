b53bb814ea38031505afae6f87d2126fcdde7643
Alfan Nur Fauzan <alpancs@gmail.com>
2015-03-28 17:18:21 +0700

    update controllers/user.php

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -8,9 +8,16 @@ class User extends CI_Controller {
 	public function profile($id){
 		$this->load->model('user_model');
 		$profile = $this->user_model->getProfile($id);
-		// print_r($profile);
-
-		$data['data_profile'] = $profile;
+		$data['profile_user_name'] = $profile['name'];
+		$data['profile_user_gender'] = $profile['gender'];
+		$data['profile_user_age'] = $profile['age'];
+		$data['profile_user_email'] = $profile['email'];
+		$data['profile_user_phone'] = $profile['phone'];
+		$data['profile_user_last_access'] = $profile['last_access'];
+		$data['profile_user_twitter'] = $profile['twitter'];
+		$data['profile_user_facebook'] = $profile['facebook'];
+		$data['profile_user_googleplus'] = $profile['googleplus'];
+		$data['profile_user_path'] = $profile['path'];
 		$this->load->model('viewer');
 		$this->viewer->show('profile_view', $data);
 	}
