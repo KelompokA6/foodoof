aa3ff1d59585835eddbdb42500fa76ac79bc1b15
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-28 07:19:17 +0700

    update template

---
+++ application/views/content_homepage.php
@@ -0,0 +1,14 @@
+<div class="col-md-9 pull-right col-no-padding-left col-no-padding-right">
+  <div class="col-md-12 col-xs-12  col-no-padding-right">
+    {carousel_highlight}
+  </div>
+  <div class="col-md-6 col-xs-12  col-no-padding-right">
+    {top_recipe_home}
+  </div>
+  <div class="col-md-6 col-xs-12 col-no-padding-left col-no-padding-right">
+    {recently_recipe_home}
+  </div>
+</div>
+<div class="col-md-3 category-menu pull-left col-no-padding-left col-no-padding-right">
+  {category_home}
+</div>
