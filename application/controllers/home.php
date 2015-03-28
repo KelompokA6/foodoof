89befa7c2a6d024100f61fcf14efd136ea7663f1
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-28 17:48:06 +0700

    update

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,53 +5,7 @@ class Home extends CI_Controller {
 	{	
 		$this->load->view('template_view');
 		//$this->load->view('top');
-	}
-	public function home1(){
-		$this->load->library('parser');
-		$this->load->library('session');
-		$loginStatus = $this->session->userdata('login_status');
-
-		$data = array();
-		if($loginStatus === 1){
-			$menubar = $this->parser->parse('menubar_login', $data, TRUE);	
-		}
-		else{
-			$menubar = $this->parser->parse('menubar_revisi', $data, TRUE);
-		}	
-		$category_home = $this->parser->parse('category_home', $data, TRUE);
-		$top_recipe_home = $this->parser->parse('top_recipe_home', $data, TRUE);
-		$recently_recipe_home = $this->parser->parse('recently_recipe_home', $data, TRUE);
-		$datacomplete = array(
-						"menubar"=> $menubar,
-						"category_home"=> $category_home,
-						"top_recipe_home"=> $top_recipe_home,
-						"recently_recipe_home"=> $recently_recipe_home,
-						);
-		$this->parser->parse('template_default_home', $datacomplete);
-	}
-	public function home2(){
-		$this->load->library('parser');
-		$this->load->library('session');
-		$loginStatus = $this->session->userdata('login_status');
-
-		$data = array();
-		if($loginStatus === 1){
-			$menubar = $this->parser->parse('menubar_login', $data, TRUE);	
-		}
-		else{
-			$menubar = $this->parser->parse('menubar_login', $data, TRUE);
-		}	
-		$category_home = $this->parser->parse('category_home', $data, TRUE);
-		$top_recipe_home = $this->parser->parse('top_recipe_home', $data, TRUE);
-		$recently_recipe_home = $this->parser->parse('recently_recipe_home', $data, TRUE);
-		$datacomplete = array(
-						"menubar"=> $menubar,
-						"category_home"=> $category_home,
-						"top_recipe_home"=> $top_recipe_home,
-						"recently_recipe_home"=> $recently_recipe_home,
-						);
-		$this->parser->parse('template_default_home', $datacomplete);
-	}
+	}	
 	public function homepage(){
 		$this->load->library('parser');
 		$this->load->library('session');
