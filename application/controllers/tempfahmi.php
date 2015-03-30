139629476734253e1c950cecb697e755b25c4979
ffahmii <ffahmii@gmail.com>
2015-03-30 17:49:04 +0700

    comit

--- application/controllers/tempfahmi.php
+++ application/controllers/tempfahmi.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Tempview extends CI_Controller {
+class Tempfahmi extends CI_Controller {
 	
 	public function recipe()
 	{	
@@ -15,27 +15,49 @@ class Tempview extends CI_Controller {
 		$this->parser->parse('template_content', $data);
 
 	}	
-	public function footer(){
-		$this->load->view('footer');
-	}
-
-	public function login()
-	{	
+	public function getR($id){
+		$recipe = new Recipe_model();
+		$user = new User_model();
+		$r = $recipe->getRecipeProfile($id);
+		$data = array();
 		$this->load->library('parser');
-		$data = array("recipe_author_id"=> 1);
 		$menubar = $this->parser->parse('menubar_login', $data, TRUE);
-		$content_website = $this->parser->parse('login_view', $data, TRUE);
+		$ingre = array();
+		foreach ($r->ingredients as $obj) {
+			$temp = array(
+					'ingre_name' => $obj->name,
+					'ingre_quantity' => $obj->quantity,
+					'ingre_units' => $obj->units,
+					'ingre_info' => $obj->info,
+				);
+			array_push($ingre, $temp);
+		}
+		//print_r($ingre);
+		$steps = array();
+		foreach ($r->steps as $obj) {
+			$temp = array(
+					'steps_number' => $obj->no_step,
+					'steps_description' => $obj->description,
+					'steps_photo' => $obj->photo,
+				);
+			array_push($steps, $temp);
+		}
+		$data = array(
+					'recipe_name' => $r->name,
+					'recipe_portion' => $r->portion,
+					'recipe_duration' => $r->duration,
+					'recipe_author_name' => $user->getProfile($r->author)['name'],
+					'recipe_last_update' => substr($r->last_update, 0, -8),
+					'recipe_ingredients' => $ingre,
+					'recipe_steps' => $steps,
+				);
+		$content_website = $this->parser->parse('recipe_view', $data, TRUE);
 		$data = array(
 					"menubar" => $menubar,
 					"content_website" => $content_website,
 				);
 		$this->parser->parse('template_content', $data);
 
-	}	
-	public function getR(){
-		$recipe = new Recipe_model();
-		$t = $recipe->getRecipeProfile(1)->name;
-		print_r($t);
 	}
 	public function search(){
 		$recipe = new Recipe_model();
