fb839c9c5e451f11485621a39c215c90c5e2e43b
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-30 18:17:11 +0700

    update

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,9 +4,13 @@
     </div>
 </footer>
 
+headernya dilebarin kaya http://ironsummitmedia.github.io/startbootstrap-freelancer/
+warna backgroundnya jadi biru foodoof (R:56 G:150 B:211) abis itu logonya jadi warna putih
+warna button search by samain kayak join
+lambang new recipe jadi warna putih
+taglinenya : infinite cooking idea
+
 konten footernya
-our amazing team;)
-liat ini http://cookpad.id/#career
-isi aja sesuai punya kita
+
 http://ironsummitmedia.github.io/startbootstrap-freelancer/
 isi aja sesuai kita wkwk
