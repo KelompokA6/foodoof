c416d57ff419ce9a6f2655978404e3ba04086150
abidnurulhakim <abidnurulhakim@gmail.com>
2015-03-31 19:27:46 +0700

    update hapus comment

--- application/views/menubar.php
+++ application/views/menubar.php
@@ -81,50 +81,6 @@
           </div>
         </div>
       </div>
-      <!-- <div class="col-md-4 navbar-collapse collapse col-menu-user">
-        <div class="col-md-2 link-by-icon" >
-          <a href="<?php echo base_url();?>recipe/create" class="text-center" title="New Recipe">
-            <i class="fa fa-pencil-square-o fa-2x"></i>
-          </a>
-        </div>
-        <div class="btn-group" role="group" aria-label="" style="padding-left: 60px;">
-          <button type="button" class="btn btn-success btn-cus" data-container="body" data-toggle="popover" data-placement="bottom" 
-          data-html="TRUE"
-          data-content="
-            <form id='loginform' class='form-horizontal' role='form' method='post' action='<?php echo base_url();?>home/login'>      
-                <div class='input-group'>
-                    <span class='input-group-addon'><i class='fa fa-user'></i></span>
-                    <input id='login-username' type='text' class='form-control' name='email_user' value='' placeholder='Email'>                                        
-                </div><br>        
-                <div class='input-group'>
-                            <span class='input-group-addon'><i class='fa fa-lock'></i></span>
-                            <input id='login-password' type='password' class='form-control' name='password_user' placeholder='Password'>
-                        </div>
-                <div style='margin-top:10px' class='form-group'>
-                    <div class='col-sm-12 controls text-center'>
-                      <button id='btn-signin' type='submit' class='btn btn-success'>Login</button>
-                    </div>
-                </div>
-                <div class='form-group'>
-                    <div class='col-md-12 control'>
-                        <div style='border-top: 1px solid#888; padding-top:15px; font-size:85%' >
-                            Forgot password? 
-                        <a href='<?php echo base_url();?>home/forgot-password'>
-                            Remember Here
-                        </a>
-                        </div>
-                    </div>
-                </div>    
-            </form>
-          "
-          >
-          Login    
-          </button>
-          <a href="<?php echo base_url();?>user/join"  class="btn-group">
-            <button type="button" class="btn btn-primary btn-cus">Join</button>
-          </a>
-        </div>
-      </div> -->
-    </div><!--/.nav-collapse -->
+    </div>
   </div>
 </nav>
