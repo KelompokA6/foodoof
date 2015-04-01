513a14babdf3c6a57cfbc0235370bf0e60d96d21
Alfan Nur Fauzan <alpancs@gmail.com>
2015-04-01 11:20:52 +0700

    KOMIT
    Gue mabok

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -14,15 +14,23 @@ class Home extends CI_Controller {
 		$listTopRecipe = $r->getTopRecipe(5);
 		$listHightlight = $r->getHightlight(5);
 		$listRecently = $r->getRecently(5);
+		$u = new User_model();
+		foreach ($listTopRecipe as $row)
+			$row->author_name = $u->getProfile($row->author)->name;
+		foreach ($listRecently as $row)
+			$row->author_name = $u->getProfile($row->author)->name;
+		foreach ($listHightlight as $row)
+			$row->author_name = $u->getProfile($row->author)->name;
+		
 		$this->home_viewer->showHome($listTopRecipe, $listHightlight, $listRecently);
 	}
 
 	public function login()
 	{
 		if ($this->session->userdata('user_id') > 0) {
-			print_r($this->session->userdata()); die;
+			redirect(base_url()); die;
 		}
-		$data = array();
+		$data['email'] = '';
 		$data['login_alert'] = '';
 		if ($this->input->server('REQUEST_METHOD') == 'POST') {
 			$data['email'] = $email = $this->input->post('email');
@@ -30,17 +38,47 @@ class Home extends CI_Controller {
 			$u = new User_model();
 			$profile = $u->login($email, $password);
 			if($profile !== FALSE) {
-				print_r($profile); die;
-				/*foreach ($profile as $key => $value) {
+				// print_r($profile); die;
+				foreach ($profile as $key => $value) {
 					$this->session->set_userdata($key, $value);
-				}*/
+				}
 				redirect(base_url());
 			} else {
-				$data['login_alert'] = 'login failed';
-				// die('failed');
+				$data['login_alert'] = '<div class="label label-danger">login failed</div>';
 			}
 		}
 		$this->home_viewer->showLogin($data);
+	}
+
+	public function logout()
+	{
+		$this->session->unset_userdata('user_id');
+		$this->session->unset_userdata('user_name');
+		$this->session->unset_userdata('user_photo');
+		redirect(base_url());
+		die;
+	}
+
+	public function toprecipe()
+	{
+		$r = new Recipe_model();
+		$listTopRecipe = $r->getTopRecipe(1001);
+		$u = new User_model();
+		foreach ($listTopRecipe as $row)
+			$row->author_name = $u->getProfile($row->author)->name;
+		
+		$this->home_viewer->showTop($listTopRecipe);
+	}
+
+	public function recently()
+	{
+		$r = new Recipe_model();
+		$listRecently = $r->getRecently(1001);
+		$u = new User_model();
+		foreach ($listRecently as $row)
+			$row->author_name = $u->getProfile($row->author)->name;
+		
+		$this->home_viewer->showRecently($listRecently);
 	}
 }
 
