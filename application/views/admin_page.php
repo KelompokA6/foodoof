2fab22ec6adfa53e23ccca80daaef65a3ce20bc6
abidnurulhakim <abidnurulhakim@gmail.com>
2015-04-12 12:47:31 +0700

    update

--- application/views/admin_page.php
+++ application/views/admin_page.php
@@ -16,96 +16,101 @@
 		  	</a>
 		  </div>
   		</div>
-  		<div class="col-md-9 col-no-padding-right page-header-title" >
-  			<h3 class="page-header"> Set Highlight Recipes </h3>
-  			<div class="col-md-2 col-no-padding-right">
-  				Choose Recipes
-  			</div>
-  			<div class="col-md-4 col-no-padding-right">
-  				<div class="list-group">
-  					<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 1</center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 2 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 3 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 4 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 5 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 6 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 7 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 8 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 9 </center>
-		  			</a>
-		  			<a href="" class="list-group-item" title="highlight setting">
-			    		<center> Recipes 10 </center>
-		  			</a>
+  		<div class="col-md-9 col-no-padding-right" >
+  			<div class="panel panel-default">
+  				<div class="panel-body">
+  					<h3 class="page-header-title"> Set Highlight Recipes </h3>
+		  			<div class="col-md-2 col-no-padding-right">
+		  				Choose Recipes
+		  			</div>
+		  			<div class="col-md-4 col-no-padding-right">
+		  				<div class="list-group">
+		  					<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 1</center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 2 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 3 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 4 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 5 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 6 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 7 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 8 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 9 </center>
+				  			</a>
+				  			<a href="" class="list-group-item" title="highlight setting">
+					    		<center> Recipes 10 </center>
+				  			</a>
+		  				</div>
+		  			</div>
+		  			<div class="col-md-6 col-no-padding-right">
+		  				<div id='div1' style='overflow:scroll;max-height:200px;display:block; border:1px'>
+				    		<div class="checkbox" >
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
+						    </label>
+						    <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 12
+						    </label>
+						     <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 13
+						    </label>
+						     <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 14
+						    </label>
+						     <label class="form-control" style='padding-left:30px'>
+						      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 25
+						    </label>
+							</div> 
+		  				</div>
+		  			</div>
   				</div>
   			</div>
-  			<div class="col-md-6 col-no-padding-right">
-  				<div id='div1' style='overflow:scroll;max-height:200px;display:block; border:1px'>
-			    		<div class="checkbox" >
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 1
-					    </label>
-					    <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 12
-					    </label>
-					     <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 13
-					    </label>
-					     <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 14
-					    </label>
-					     <label class="form-control" style='padding-left:30px'>
-					      	<input type="checkbox" class="checkedHighlight" value='{highlighted_recipe_id}' {checked_status}> Name Recipes 25
-					    </label>
-						</div> 
-  				</div>
-  			</div>
+  			
   		</div>
 	</div>	
 	<div class="col-md-12 text-center">
