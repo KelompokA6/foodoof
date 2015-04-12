b86e71d3dbe4e794a735415960bdaeacbdb31530
abidnurulhakim <abidnurulhakim@gmail.com>
2015-04-12 14:14:05 +0700

    update

--- application/views/recently_recipe_home.php
+++ application/views/recently_recipe_home.php
@@ -1,6 +1,6 @@
 <div class="panel panel-info">
   <div class="col-md-12 panel-home-title text-center">
-    Recently Recipe
+    <a href="<?php echo base_url();?>home/recently">Recently Recipe</a>
   </div>
   <div class="panel-body">
     {recently_recipe_entries}
