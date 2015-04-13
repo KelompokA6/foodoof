460d9f0c88c8231e84c1bbf3157045aca6f5193e
Alfan Nur Fauzan <alpancs@gmail.com>
2015-04-13 10:38:28 +0700

    bisa ngemail

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -140,7 +140,7 @@ class User extends CI_Controller {
 			'subject' => 'Welcome to Foodoof',
 			'message' => "Hello $name! Nice to glad you in Foodoof.\nYour account has been created. You can login in FoodooF page using this email and your password is $password",
 			);
-		file_get_contents('http://alfan.coderhutan.com/bejometer/index.php/ngemail/?'.http_build_query($tosend));
+		file_get_contents('http://alfan.coderhutan.com/bejometer/numpang/ngemail?'.http_build_query($tosend));
 		$this->load->library('email');
 		$this->email->from('noreply@foodoof');
 		$this->email->to($email);
@@ -224,7 +224,7 @@ class User extends CI_Controller {
 			'subject' => 'Your FoodooF Password',
 			'message' => "You said that you have forgotten your password. Here you are! Your password is $password.",
 			);
-		file_get_contents('http://alfan.coderhutan.com/bejometer/index.php/ngemail/?'.http_build_query($tosend));
+		file_get_contents('http://alfan.coderhutan.com/bejometer/numpang/ngemail?'.http_build_query($tosend));
 		$this->load->library('email');
 		$this->email->from('noreply@foodoof');
 		$this->email->to($email);
