52bff5b6607e3bc058e468ffcd7e7cc2aa373cf2
abidnurulhakim <abidnurulhakim@gmail.com>
2015-04-13 08:17:57 +0700

    update category home

--- application/views/category_home.php
+++ application/views/category_home.php
@@ -6,7 +6,7 @@
     <div class="col-md-12 col-xs-12 hr-list-item category-list">
       <a href="<?php echo base_url();?>recipe/category/Indonesian%20Food">
         <div class="col-md-3 col-xs-5">
-          <img class="img-responsive img-circle" src="<?php echo base_url();?>assets/img/foodoof-vs.png"/>
+          <img class="img-responsive img-circle" src="<?php echo base_url();?>assets/img/category/indonesian-food.jpg"/>
         </div>
         <div class="col-md-8 col-xs-6 category-menu-name">
           Indonesian Food
@@ -17,7 +17,7 @@
     <div class="col-md-12 col-xs-12 page-header hr-list-item category-list">
       <a href="<?php echo base_url();?>recipe/category/rice">
         <div class="col-md-3 col-xs-5 img-category">
-          <img class="img-responsive img-rounded" src="<?php echo base_url();?>assets/img/sate-ayam.jpg"/>
+          <img class="img-responsive img-rounded" src="<?php echo base_url();?>assets/img/category/rice.jpg"/>
         </div>
         <div class="col-md-8 col-xs-6 category-menu-name">
           Rice
