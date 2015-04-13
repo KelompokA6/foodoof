9db13e1f52d528000620c3b2ddca6594796fd351
ffahmii <ffahmii@gmail.com>
2015-04-13 08:27:35 +0700

    update

--- application/views/category_home.php
+++ application/views/category_home.php
@@ -5,7 +5,7 @@
   <div class="panel-body panel-category" for="category">
     <div class="col-md-12 col-xs-12 hr-list-item category-list">
       <a href="<?php echo base_url();?>recipe/category/Indonesian%20Food">
-        <div class="col-md-3 col-xs-5">
+        <div class="col-md-3 col-xs-5 img-category">
           <img class="img-responsive img-rounded" src="<?php echo base_url();?>assets/img/category/indonesian-food.jpg"/>
         </div>
         <div class="col-md-8 col-xs-6 category-menu-name">
