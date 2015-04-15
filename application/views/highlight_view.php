7a0c6db6b9bd3ed23d0005ca62b256e5994e3015
ffahmii <ffahmii@gmail.com>
2015-04-15 12:31:48 +0700

    update

--- application/views/highlight_view.php
+++ application/views/highlight_view.php
@@ -29,7 +29,7 @@
       </div>
       <div class="col-md-12 col-xs-12 details">
         <div class="col-md-11" title="Rating">
-            <input id="input-2b" class="rating" value="{highlight_recipe_rating}" data-readonly='true' data-min="0" data-max="5" data-starts=3 data-step="0.1" data-stars=5 data-symbol="&#xe005;" data-size="xs" data-default-caption="{rating} hearts" data-star-captions="{}" data-show-clear="false">
+            <input id="input-2b" class="rating" value="{highlight_recipe_rating}" data-recipeId="{highlight_recipe_id}" data-readonly='true' data-min="0" data-max="5" data-starts=3 data-step="0.1" data-stars=5 data-symbol="&#xe005;" data-size="xs" data-default-caption="{rating} hearts" data-star-captions="{}" data-show-clear="false">
         </div>
       </div>  
     </div>
