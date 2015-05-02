a17aaf3da66c113bb8a7e7f802f33bd0b41c9be6
abidnurulhakim <abidnurulhakim@gmail.com>
2015-05-02 12:01:29 +0700

    update online user

--- application/views/template_content.php
+++ application/views/template_content.php
@@ -31,7 +31,7 @@
     	  		</div>
     	  	</div>
     	</div>
-      <div id="users-online" class="btn btn-default col-md-2 col-no-padding text-left" style="position:fixed; left:0; bottom:0; z-index:100">
+      <div id="users-online" class="btn btn-default col-md-2 col-xs-4 col-no-padding text-left" style="position:fixed; left:0; bottom:0; z-index:100">
         <div id="panel-users" class="panel panel-default">
           <div class="panel-heading">
             <h3 class="panel-title">10 Users Online</h3>
@@ -79,12 +79,12 @@
             </div>
           </div>
         </div>
-        <div class="col-md-12 col-no-padding" id="toggle-online-user">
-          <div class="col-md-9 text-left">
+        <div class="col-md-12 col-xs-12 col-no-padding" id="toggle-online-user">
+          <div class="col-md-9 col-xs-8 text-left">
             <i class="fa fa-user fa-2x"></i>
             <span style="padding-left:15px;font-size:18px"> 10 Online</span>
           </div>
-          <div class="col-md-2">
+          <div class="col-md-2 col-xs-4">
             <i class="fa fa-chevron-up fa-2x"></i>
           </div>
         </div>
