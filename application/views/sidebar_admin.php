9f8b07fc6e0cdbacdefb80a1a6bfc52e906fa6e4
abidnurulhakim <abidnurulhakim@gmail.com>
2015-05-04 16:23:15 +0700

    update

--- application/views/sidebar_admin.php
+++ application/views/sidebar_admin.php
@@ -1,18 +1,18 @@
-<div class="col-md-12 col-xs-12">
+<div class="col-md-12 col-xs-12 col-no-padding">
 	<div class="list-group">
 	  	<a href="" class="list-group-item" title="highlight setting">
 			<center> Highlight Recipes </center>
 	  	</a>
-	  	<a href="" class="list-group-item disabled" title="broadcast">
+	  	<a href="" class="list-group-item" title="broadcast">
 			<center> Broadcast Message </center>
 		</a>
-	  	<a href="" class="list-group-item disabled" title="reported">
+	  	<a href="" class="list-group-item" title="reported">
 			<center> Reported User </center>
 	  	</a>
-	  	<a href="" class="list-group-item disabled" title="reported">
+	  	<a href="" class="list-group-item" title="Email User">
 			<center> Send Email To User </center>
 	  	</a>
-	  	<a href="" class="list-group-item disabled" title="reported">
+	  	<a href="" class="list-group-item" title="Update Catalog">
 			<center> Update Catalog </center>
 	  	</a>
   	</div>
