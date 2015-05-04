a464b72467815e68f3770b4e8971acf5c80913b5
abidnurulhakim <abidnurulhakim@gmail.com>
2015-05-04 16:22:45 +0700

    update report admin

--- application/views/reported_user_view.php
+++ application/views/reported_user_view.php
@@ -1,107 +1,161 @@
 <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
-	<div class="col-md-12 col-xs-12 col-no-padding-right">
+	<div class="col-md-12 col-xs-12 col-no-padding">
 		<div class="panel panel-default">
 			<div class="panel-body">
 				<h3 class="page-header-title"> Reported User </h3>
 				<?php
 					echo $this->session->flashdata('alert-admin');
 				?>
-  				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
-  					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
-				      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
-				      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
-							<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
+					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
+			      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
+			      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
+						<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+					</div>
+					<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
+							Abid Nurul Hakim
 						</div>
-						<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
-  							Abid Nurul Hakim
-  						</div>
-				    </label>
-  					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
-  						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
-  					</div>
-  					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
-  						Anda memiliki no report</div>
-  				</div>
-  				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
-  					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
-				      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
-				      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
-							<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+			    </label>
+					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
+						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
+					</div>
+					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
+            <ul class="list-group">
+              <li class="col-md-12 col-sm-12 col-xs-12 col-no-padding">
+                <div class="col-md-1 col-xs-1 col-sm-1">
+                  1
+                </div>
+                <div class="col-md-5 col-xs-5 col-sm-5 col-no-padding">
+                  Ayam Goreng
+                </div>
+                <div class="col-md-6 col-xs-6 col-sm-6">
+                  SPAM, Advertase
+                </div>
+              </li>
+              <li class="col-md-12 col-sm-12 col-xs-12 col-no-padding">
+                <div class="col-md-1 col-xs-1 col-sm-1">
+                  5
+                </div>
+                <div class="col-md-5 col-xs-5 col-sm-5 col-no-padding">
+                  Es Buah
+                </div>
+                <div class="col-md-6 col-xs-6 col-sm-6">
+                  Advertase, Rasis
+                </div>
+              </li>
+              <li class="col-md-12 col-sm-12 col-xs-12 col-no-padding">
+                <div class="col-md-1 col-xs-1 col-sm-1">
+                  11
+                </div>
+                <div class="col-md-5 col-xs-5 col-sm-5 col-no-padding">
+                  curry rice
+                </div>
+                <div class="col-md-6 col-xs-6 col-sm-6">
+                  SARA, Plagiat
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
+					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
+			      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
+			      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
+						<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+					</div>
+					<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
+							Fahmi Rahmat K
 						</div>
-						<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
-  							Fahmi Rahmat K
-  						</div>
-				    </label>
-  					<<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
-  						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
-  					</div>
-  					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
-  						Anda memiliki no report</div>
-  				</div>
-  				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
-  					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
-				      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
-				      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
-							<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+			    </label>
+					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
+						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
+					</div>
+					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
+						Anda memiliki no report</div>
+				</div>
+				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
+					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
+			      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
+			      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
+						<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+					</div>
+					<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
+							Agnes Agustina
 						</div>
-						<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
-  							Agnes Agustina
-  						</div>
-				    </label>
-  					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
-  						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
-  					</div>
-  					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
-  						Anda memiliki no report</div>
-  				</div>
-  				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
-  					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
-				      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
-				      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
-							<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+			    </label>
+					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
+						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
+					</div>
+					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
+						<ul class="list-group">
+              <li class="col-md-12 col-sm-12 col-xs-12 col-no-padding">
+                <div class="col-md-1 col-xs-1 col-sm-1">
+                  1
+                </div>
+                <div class="col-md-5 col-xs-5 col-sm-5 col-no-padding">
+                  Ayam Goreng
+                </div>
+                <div class="col-md-6 col-xs-6 col-sm-6">
+                  SPAM, Advertase
+                </div>
+              </li>
+              <li class="col-md-12 col-sm-12 col-xs-12 col-no-padding">
+                <div class="col-md-1 col-xs-1 col-sm-1">
+                  5
+                </div>
+                <div class="col-md-5 col-xs-5 col-sm-5 col-no-padding">
+                  Es Buah
+                </div>
+                <div class="col-md-6 col-xs-6 col-sm-6">
+                  Advertase, Rasis
+                </div>
+              </li>
+              <li class="col-md-12 col-sm-12 col-xs-12 col-no-padding">
+                <div class="col-md-1 col-xs-1 col-sm-1">
+                  11
+                </div>
+                <div class="col-md-5 col-xs-5 col-sm-5 col-no-padding">
+                  curry rice
+                </div>
+                <div class="col-md-6 col-xs-6 col-sm-6">
+                  SARA, Plagiat
+                </div>
+              </li>
+            </ul>
+          </div>
+				</div>
+				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
+					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
+			      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
+			      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
+						<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+					</div>
+					<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
+							Alfan Nur Fauzan
 						</div>
-						<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
-  							Alfan Nur Fauzan
-  						</div>
-				    </label>
-  					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
-  						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
-  					</div>
-  					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
-  						Anda memiliki no report</div>
-  				</div>
-  				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
-  					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
-				      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
-				      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
-							<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+			    </label>
+					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
+						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
+					</div>
+					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
+						Anda memiliki no report</div>
+				</div>
+				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
+					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
+			      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
+			      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
+						<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
+					</div>
+					<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
+							Jean Fathanah
 						</div>
-						<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
-  							Jean Fathanah
-  						</div>
-				    </label>
-  					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
-  						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
-  					</div>
-  					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
-  						Anda memiliki no report</div>
-  				</div>
-  				<div class="col-md-12 col-xs-12 col-sm-12 border-solid-bottom" style="padding:5px 0">
-  					<label class=" col-md-9 col-xs-9 col-sm-3 control-label text-left">
-				      	<input type="checkbox" class="col-md-1 col-xs-1 col-sm-1 checkboxuser" name= 'id_reported[]' value='{report_user_id}' {highlight_checked}>
-				      	<div class="col-md-1 col-xs-2 col-sm-1 col-no-padding">
-							<img src="http://localhost/foodoof/assets/img/user-male.png" class="img-responsive img-rounded img-report">
-						</div>
-						<div class="col-md-8 col-xs-8 col-sm-8 col-no-padding user-report text-left">
-  							Admin 
-  						</div>
-				    </label>
-  					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
-  						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
-  					</div>
-  					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
-  						Anda memiliki no report</div>
-  				</div>
+			    </label>
+					<div class="col-md-3 col-xs-3 col-sm-3" style="padding-top:7px">
+						<div class="btn button-secondary details-button">Details <i class="fa fa-chevron-right"></i></div>
+					</div>
+					<div class='col-md-offset-1 col-md-11 col-xs-11 col-sm-11 details-reported' style='padding:5px 0 5px 44px'>
+						Anda memiliki no report</div>
+				</div>
 			</div>
 		</div>
 	</div>	
--- application/views/template_admin.php
+++ application/views/template_admin.php
@@ -1,6 +1,6 @@
 <div  class="col-md-9 col-xs-12 pull-right text-left" style="margin-bottom:20px">
 	{content_admin}
 </div>
-<div class="col-md-3 col-xs-12">
+<div class="col-md-3 col-xs-12 pull-left">
 	{sidebar_admin}
 </div>
