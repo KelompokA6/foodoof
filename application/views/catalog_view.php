d21e6238043047a93e738e32527d72cc2434f0c9
jean fathanah <je.fathanah@gmail.com>
2015-05-11 11:12:13 +0700

    catalog

--- application/views/catalog_view.php
+++ application/views/catalog_view.php
@@ -5,7 +5,7 @@
 			<?php
 				echo $this->session->flashdata('alert-admin');
 			?>
-			<div class="col-md-12 col-xs-12 col-sm-12 col-no-padding border-solid-bottom">
+			<div class="col-md-12 col-xs-12 col-sm-12 col-no-padding border-solid-bottom" style="margin:5px 0 15px 0">
 				<div class="col-md-12 col-xs-12 col-sm-12 col-no-padding-left text-right">
 					<div id="add-entry-catalog" class="btn button-primary">
 						<i class="fa fa-plus"></i> <span>Add Entry</span>
@@ -29,28 +29,48 @@
 			    	</div>
 				</form>
 			</div>
-			<form class="form-horizontal col-md-12 col-xs-12 col-sm-12 border-solid-bottom catalog-list" action="<?php echo base_url();?>index.php/admin/update"role="form" method="post" enctype="multipart/form-data" style="padding:15px">
-				{catalog_entries}
-				<div class="col-md-12 col-xs-12 col-sm-12 col-no-padding" style="margin:2px 0;">
-					<div class="col-md-3 col-sm-3 col-xs-3">
-						<input type="text" maxlength="254" value="{catalog_id}" name="catalog_id[]" class="form-control input-ingredient hidden" placeholder="Ingredient Name">
-			      		<input type="text" maxlength="254" value="{catalog_name}" name="catalog_name[]" class="form-control input-ingredient" placeholder="Ingredient Name">
-			    	</div>
-			    	<div class="col-md-3 col-sm-3 col-xs-3 col-no-padding-left">
-			      		<input type="text" maxlength="254" value="{catalog_unit}" name="catalog_unit[]" class="form-control ingredient-unit" placeholder="Unit" autocomplete="off">
-			    	</div>
-			    	<div class="col-md-3 col-sm-3 col-xs-3 col-no-padding-left">
-			      		<input type="number" min="0" step="0.01" value="{catalog_quantity}" name="catalog_quantity[]" class="form-control" placeholder="Quantity">
-			    	</div>
-			    	<div class="col-md-3 col-sm-3 col-xs-3 col-no-padding-left">
-			      		<input type="number" min="0" step="1" value="{catalog_price}" name="catalog_price[]" class="form-control" placeholder="Price">
-			    	</div>
-				</div>
-				{/catalog_entries}
-				<div class="col-md-12 col-sm-12 col-xs-12 text-center">
-		    		<button type="submit" class="btn button-primary">Update</button>
-		    	</div>
-			</form>
+			<!-- <form class="form-horizontal col-md-12 col-xs-12 col-sm-12 border-solid-bottom catalog-list" role="form" method="post" enctype="multipart/form-data" style="padding:15px"> -->
+			<table id="catalog-table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+		        <thead>
+		            <tr>
+		                <th>ID</th>
+		                <th>Name</th>
+		                <th>Unit</th>
+		                <th>Quantity</th>
+		                <th>Price</th>
+		            </tr>
+		        </thead>
+		        <tfoot>
+		            <tr>
+		                <th>ID</th>
+		                <th>Name</th>
+		                <th>Unit</th>
+		                <th>Quantity</th>
+		                <th>Price</th>
+		            </tr>
+		        </tfoot>
+		        {catalog_entries}
+		        <tr>
+	                <td>{catalog_id}</td>
+	                <td class="edit" data-type="text" data-name="name" data-pk="{catalog_id}" data-title="Enter Name Catalog">
+	                	{catalog_name}
+	                </td>
+	                <td class="edit" data-type="text" data-name="units" data-pk="{catalog_id}" data-title="Enter Unit Catalog">
+	                	{catalog_unit}
+	                </td>
+	                <td class="edit" data-type="text" data-name="quantity" data-pk="{catalog_id}" data-title="Enter Quantity Catalog">
+	                	{catalog_quantity}
+	                </td>
+	                <td class="edit" data-type="text" data-name="price" data-pk="{catalog_id}" data-title="Enter Price Catalog">
+	                	{catalog_price}
+	                </td>
+	            </tr>
+		        {/catalog_entries}
+		    </table>
+			<!-- </form> -->
+			<div class="col-md-12 col-sm-12 col-xs-12 text-center">
+	    		<button type="submit" class="btn button-primary">Update</button>
+	    	</div>
 		</div>
 	</div>
 </div>	
