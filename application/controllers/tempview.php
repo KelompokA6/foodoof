33fa3bd2232234696bfd1e02050de7560cd737af
morcaw <morcaw@live.com>
2015-05-11 11:35:31 +0700

    coba lg

--- application/controllers/tempview.php
+++ application/controllers/tempview.php
@@ -401,29 +401,38 @@ class Tempview extends CI_Controller {
   	/*
 		ambil resep2 user yg dilaporin, ambil detailnya di recipe_model
   	*/
-	$recipe = new report();
-	$list = $recipe -> getListReportByUserId();
-	foreach ($list as $obj) {
-		# code...
-		$detilres = new Recipe_model();
-		$detil = $detilres ->  getDetailsReportByRecipeId($obj->id);
-		$details = array();
-		foreach ($detil as $obj) {
-		 	# code...
-		 	$temp = array(
-		 		'reported_recipe_name' => $obj->recipe_id, 
-		 		'reported_recipe_count_ads' => $obj->cads,
-		 		'reported_recipe_count_porn' => $obj->cporn,
-		 		'reported_recipe_count_spam' => $obj->cspam,
-		 		'reported_recipe_count_other' => $obj->cother,
-		 		'reported_recipe_other_entries' => $obj->list_details_report
-		 	);
-		 	array_push($details, $temp);
-		} 
+	$user = new User_model();
+	$listRepo = $user -> getListReportUser();
+	foreach ($listRepo as $objRepo => $value) {
+		$recipe = new report();
+		$list = $recipe -> getListReportByUserId($objRepo->id);
+		$namauser = array();
+		foreach ($list as $obj) {
+			# code...
+			$temp1 = array('reported_user_name' => $obj->name);
+			$detilres = new Recipe_model();
+			$detil = $detilres ->  getDetailsReportByRecipeId($obj->id);
+			$details = array();
+			foreach ($detil as $obj) {
+			 	# code...
+			 	$temp = array(
+			 		'reported_recipe_name' => $obj->recipe_id, 
+			 		'reported_recipe_count_ads' => $obj->cads,
+			 		'reported_recipe_count_porn' => $obj->cporn,
+			 		'reported_recipe_count_spam' => $obj->cspam,
+			 		'reported_recipe_count_other' => $obj->cother,
+			 		'reported_recipe_other_entries' => $obj->list_details_report
+			 	);
+			 	array_push($details, $temp);
+			}
+			array_push($namauser, $temp1) 
+		}
+		$data1 = array(
+			'reported_recipe_entries' => $details,
+			'reported_user_entries' => $namauser
+			);
+		$reported_user_view = $this->parser->parse('reported_user_view', $data1, TRUE);	
 	}
-	$data1 = array(
-		'reported_recipe_entries' => $details,
-		);
-	$reported_user_view = $this->parser->parse('reported_user_view', $data1, TRUE);	
+	
   }
 }
