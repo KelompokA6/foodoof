c0360e03324075eaca1f906290456f38bcbcddc0
abidnurulhakim <abidnurulhakim@gmail.com>
2015-05-20 10:17:24 +0700

    update

--- assets/js/foodoof.js
+++ assets/js/foodoof.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 	$lock = 0;
 	$submitStatus=false;
 	$hasChanged = false;
-	$baseurl = window.location.hostname+"/index.php";
-	$baseurlnoConflict = window.location.hostname;
+	$baseurl = "http://localhost/foodoof/index.php";
+	$baseurlnoConflict = "http://localhost/foodoof/";
 	$.get( $baseurl+"/processAjax/schedulercleantmp", function( data ) {
 	},"json");
 	/*
